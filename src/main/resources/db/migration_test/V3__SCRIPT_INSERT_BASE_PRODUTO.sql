e4decbeef0f98057004eead277d1e1315c863a3c
jonassantos1000 <jonasvale3@hotmail.com>
2023-12-19 00:12:34 -0300

    fix: ajustado migrations de teste

---
+++ src/main/resources/db/migration_test/V3__SCRIPT_INSERT_BASE_PRODUTO.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_fornecedor (IDENT, ds_nome,dt_atualizacao,id_tenant,id_usuario_atualizacao) VALUES
+	 (999, 'fornecedor 2','2023-12-18 22:56:02.297703-03','a7efdd20-e12b-48fe-81fe-0b109db5da95',1);
+
+INSERT INTO tb_categoria (IDENT, ds_descricao,id_tenant,id_usuario_atualizacao,dt_atualizacao) VALUES
+	 (999, 'string','a7efdd20-e12b-48fe-81fe-0b109db5da95',1,'2023-12-17 21:02:35.887162-03');
